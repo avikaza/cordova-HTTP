6556620818007f6d6bab3502ae2d1c17f7a4fb97
Madhu Benidi <Madhu.Benidi@raritan.com>
2015-08-18 13:24:58 -0400

    Changes for supporting PUT, DELETE methods in CordovaHttpPlugin for Android.

--- src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
@@ -73,7 +73,23 @@ public class CordovaHttpPlugin extends CordovaPlugin {
              HashMap<String, String> headersMap = this.addToMap(this.globalHeaders, headers);
              CordovaHttpPostJson postJson = new CordovaHttpPostJson(urlString, jsonObj, headersMap, callbackContext);
              cordova.getThreadPool().execute(postJson);
-         } else if (action.equals("useBasicAuth")) {
+        } else if (action.equals("put")) {
+            String urlString = args.getString(0);
+            JSONObject params = args.getJSONObject(1);
+            JSONObject headers = args.getJSONObject(2);
+            HashMap<?, ?> paramsMap = this.getMapFromJSONObject(params);
+            HashMap<String, String> headersMap = this.addToMap(this.globalHeaders, headers);
+            CordovaHttpPut put = new CordovaHttpPut(urlString, paramsMap, headersMap, callbackContext);
+            cordova.getThreadPool().execute(put);
+        } else if (action.equals("delete")) {
+            String urlString = args.getString(0);
+            JSONObject params = args.getJSONObject(1);
+            JSONObject headers = args.getJSONObject(2);
+            HashMap<?, ?> paramsMap = this.getMapFromJSONObject(params);
+            HashMap<String, String> headersMap = this.addToMap(this.globalHeaders, headers);
+            CordovaHttpDelete delete = new CordovaHttpDelete(urlString, paramsMap, headersMap, callbackContext);
+            cordova.getThreadPool().execute(delete);
+        } else if (action.equals("useBasicAuth")) {
             String username = args.getString(0);
             String password = args.getString(1);
             this.useBasicAuth(username, password);
